10a0bc58f21d0ef2218ee68bc3e24ce86adf0725
mega2799 <megadigimatt@gmail.com>
2020-12-07 23:07:23 +0100

    Completed needs a rewiew

--- src/it/unibo/oop/lab/reactivegui03/AnotherConcurrentGUI.java
+++ src/it/unibo/oop/lab/reactivegui03/AnotherConcurrentGUI.java
@@ -1,14 +1,18 @@
-package it.unibo.oop.lab.reactivegui02;
+package it.unibo.oop.lab.reactivegui03;
 
 import java.awt.FlowLayout;
+import java.lang.reflect.InvocationTargetException;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
+
+import it.unibo.oop.lab.reactivegui02.ConcurrentGUI;
+
 import javax.swing.JButton;
 
-public class ConcurrentGUI extends JFrame {
+public class AnotherConcurrentGUI extends JFrame {
 
     /**
      * 
@@ -16,14 +20,17 @@ public class ConcurrentGUI extends JFrame {
     private static final long serialVersionUID = -5809059974380444922L;
     private static int X = 800;
     private static int Y = 600;
+    private static int CLOCK_END = 10;
 
     private JPanel panel = new JPanel(new FlowLayout());
     private JLabel label = new JLabel();
     private final JButton upButton = new JButton("UP");
     private final JButton downButton = new JButton("DOWN");
     private final JButton stopButton = new JButton("STOP");
+    private int timer;
 
-    public ConcurrentGUI() {
+    final Agent agent = new Agent();
+    public AnotherConcurrentGUI() {
         super("Concurrent GUI");
         this.setSize(X, Y);
         panel.add(label);
@@ -34,21 +41,23 @@ public class ConcurrentGUI extends JFrame {
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setVisible(true);
 
-        final Agent agent = new Agent();
+        final Clock clock = new Clock();
+        new Thread(clock).start();
         new Thread(agent).start();
 
         upButton.addActionListener(e -> agent.swtichOperation(true));
         downButton.addActionListener(e -> agent.swtichOperation(false));
         stopButton.addActionListener(e -> {
-                agent.stopRunning();
-                downButton.setEnabled(false);
-                upButton.setEnabled(false);
-                stopButton.setEnabled(false);
+                dryStop();
             });
     }
-
+    public void dryStop() {
+        agent.stopRunning();
+        AnotherConcurrentGUI.this.upButton.setEnabled(false);
+        AnotherConcurrentGUI.this.downButton.setEnabled(false);
+        AnotherConcurrentGUI.this.stopButton.setEnabled(false);
+    }
     public class Agent implements Runnable {
-
         private volatile int runCount;
         private volatile boolean isAdding;
         private volatile boolean isRunning;
@@ -78,13 +87,28 @@ public class ConcurrentGUI extends JFrame {
             while (isRunning) {
                 try {
                     Thread.sleep(100);
-                    SwingUtilities.invokeLater(() -> ConcurrentGUI.this.label.setText(this.applyOperation(runCount)));
+                    SwingUtilities.invokeLater(() -> AnotherConcurrentGUI.this.label.setText(this.applyOperation(runCount)));
                 } catch (InterruptedException e) {
-                    // TODO Auto-generated catch block
                     e.printStackTrace();
                 }
             }
         }
     }
 
+    public class Clock implements Runnable { 
+
+        private int timer;
+        @Override
+        public void run() {
+           while (this.timer < CLOCK_END) {
+              try {
+                Thread.sleep(1000);
+                this.timer++;
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+           }
+           dryStop();
+        }
+    }
 }
